682b5afbffc2b59083ffb61ece720a7d38aa2fbe
Pedro-Calderon <s18120164@alumnos.itsur.edu.mx>
2025-01-12 19:37:22 -0600

    Agregamos footer

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import HelloWorld from './Components/HelloWorld';
 import CounterExample from './Components/CounterExample';
 import Header from './Components/Header';
+import Footer from './Components/Footer';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
     <div>
       <Header/>
       <HelloWorld name="Pedro"/>
+      <Footer/>
 
      { /*<CounterExample/>*/}
     </div>
